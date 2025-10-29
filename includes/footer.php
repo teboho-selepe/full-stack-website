6966515c1c246299e5e6db07710653ab96dc3df0
Teboho <tejahteboho@gmail.com>
2025-10-29 12:52:52 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,17 @@
     <footer class="site-footer">
-        <table style="width:100%;border-collapse:collapse; background: rgba(3, 3, 3, 0.8);">
+        <table style="width:100%;border-collapse:collapse; background: rgba(3, 3, 3, 0.8); backdrop-filter: blur(20px);">
             <tr>
                 <td style="padding:16px 0;text-align:left;vertical-align:top;width:33%;">
                     <img src="/web/assets/logo.png" alt="SmartPath Tutoring Logo" style="height:200px; display:block; margin-bottom:8px;">
                 </td>
 
-                <td style="padding:16px 0;text-align:center;vertical-align:top;width:33%;">
+                <td style="padding:16px 0;text-align:center;vertical-align:top;width:33%; color:#fff;">
                     <strong>Quick Links</strong><br>
-                    <a href="/web/index.php">Home</a> |
-                    <a href="/web/pages/about.php">About</a> |
-                    <a href="/web/pages/contact.php">Contact</a>
+                    <span><a href="/web/index.php" class="footer-link">Home</a></span><br>
+                    <span><a href="/web/pages/about.php" class="footer-link">About</a></span><br>
+                    <span><a href="/web/pages/contact.php" class="footer-link">Contact</a></span>
                 </td>
-                <td style="padding:16px 0;text-align:right;vertical-align:top;width:33%;">
+                <td style="padding:16px ;text-align:right;vertical-align:top;width:33%; color:#fff;" >
                     <strong>Contact</strong><br>
                     <span style="font-size:14px;">Email: info@smartpath.com</span><br>
                     <span style="font-size:14px;">Phone: +27 12 345 6789</span>
